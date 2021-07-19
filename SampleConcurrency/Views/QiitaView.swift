4230930e583a07b2818782c356b7e7f64206b7a8
fuwamaki <30719264+fuwamaki@users.noreply.github.com>
2021-07-19 17:08:32 +0900

    delete unnecessary code in QiitaView

--- SampleConcurrency/Views/QiitaView.swift
+++ SampleConcurrency/Views/QiitaView.swift
@@ -9,16 +9,10 @@ import SwiftUI
 
 struct QiitaView: View {
     @ObservedObject var viewModel: SingleViewModel
-//    @Binding var selectedType: SingleItemType
-//    @Binding var searchText: String
-//    @Binding var isSearching: Bool
 
-    private let apiClient = APIClient()
-    @State private var items: [QiitaItem] = []
     @State private var isLoading: Bool = false
     @State private var isShowAlert: Bool = false
     @State private var alertMessage: String = ""
-    @State private var searchedText: String = ""
 
     var body: some View {
         ZStack {
@@ -31,11 +25,6 @@ struct QiitaView: View {
                     .tint(.cyan)
             }
         }
-//        .onChange(of: isSearching, perform: { newValue in
-//            if newValue, selectedType == .qiita {
-//                fetch()
-//            }
-//        })
         .alert(isPresented: $isShowAlert) {
             Alert(
                 title: Text("エラー"),
@@ -45,13 +34,11 @@ struct QiitaView: View {
         .onReceive(viewModel.$searchText) { text in
             fetch(text)
         }
-//        .onAppear {
-//            fetch()
-//        }
     }
 
     func fetch(_ text: String) {
         async {
+            isLoading = true
             do {
                 try await viewModel.fetchQiitaItem(text: text)
             } catch let error {
@@ -60,25 +47,9 @@ struct QiitaView: View {
                     isShowAlert.toggle()
                 }
             }
+            isLoading = false
         }
     }
-//    func fetch() {
-//        guard searchText.count > 0, searchedText != searchText else { return }
-//        async {
-//            isLoading = true
-//            do {
-//                items = try await apiClient
-//                    .fetchQiitaItem(url: APIUrl.qiitaItem(query: searchText))
-//                searchedText = searchText
-//            } catch let error {
-//                if let apiError = error as? APIError {
-//                    alertMessage = apiError.message
-//                    isShowAlert.toggle()
-//                }
-//            }
-//            isLoading = false
-//        }
-//    }
 }
 
 struct QiitaView_Previews: PreviewProvider {
